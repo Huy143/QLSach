a6749bf6f82ce6c2956b26aea18e3ac6aa89e7d0
HUY <ngquochuy12c3@gmail.com>
2023-05-21 23:01:43 +0700

    git config --global user.name "Huy143"

---
+++ src/main/java/nguyenquochuy/demo/repository/ICategoryRepository.java
@@ -0,0 +1,10 @@
+package nguyenquochuy.demo.repository;
+
+
+import nguyenquochuy.demo.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
+}
